cfbf3fcab0674e20fba50040319d147a41886991
JFKTBonny <jkamkotoyip@yahoo.com>
2025-11-22 16:31:39 +0100

    update repo

--- scripts/deploy-infra.sh
+++ scripts/deploy-infra.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
+set -e  # exit on any error
 
-STACK_NAME=awsbootstrap 
-REGION=us-east-1 
-CLI_PROFILE=default 
-
-EC2_INSTANCE_TYPE=t2.micro 
+STACK_NAME="awsbootstrap"
+REGION="us-east-1"
+CLI_PROFILE="default"
+TEMPLATE_FILE="../templates_files/main.yaml"
+EC2_INSTANCE_TYPE="t2.micro"
 
 # Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
+echo -e "\n\n=========== Deploying main.yaml ==========="
 aws cloudformation deploy \
---region $REGION \
---profile $CLI_PROFILE \
---stack-name $STACK_NAME \
---template-file main.yml \ 
---no-fail-on-empty-changeset \
---capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides \ 
-EC2InstanceType=$EC2_INSTANCE_TYPE
+    --region "$REGION" \
+    --profile "$CLI_PROFILE" \
+    --stack-name "$STACK_NAME" \
+    --template-file "$TEMPLATE_FILE" \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides EC2InstanceType="$EC2_INSTANCE_TYPE"
 
 # If the deploy succeeded, show the DNS name of the created instance
-if [ $? -eq 0 ]; then
+echo -e "\nFetching instance endpoint..."
 aws cloudformation list-exports \
---profile awsbootstrap \
---query "Exports[?Name=='InstanceEndpoint'].Value" # outputs the url (endpoint) of the instance...
-fi
+    --profile "$CLI_PROFILE" \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" \
+    --output text
